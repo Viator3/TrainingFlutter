8228412cd93796888046de88aa0f66b70a545045
Valerii <valeradots81@gmail.com>
2021-03-02 15:19:52 +0200

    align_class_work_1

--- classroom_work_1/lib/main.dart
+++ classroom_work_1/lib/main.dart
@@ -10,47 +10,47 @@ class MyClassroomWork1 extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          backgroundColor: Colors.teal,
-          body: SafeArea(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  width: 100,
-                  color: Colors.red,
-                ),
-                Expanded(
-                  child: Container(
-                    color: Colors.teal,
-                    child: Center(
-                      child: Container(
-                        width: 100,
-                        height: 200,
-                        child: Column(
-                          children: [
-                            Expanded(
-                              child: Container(
-                                color: Colors.amber,
-                              ),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 100,
+                color: Colors.red,
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.teal,
+                  child: Center(
+                    child: Container(
+                      width: 100,
+                      height: 200,
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              color: Colors.amber,
                             ),
-                            Expanded(
-                              child: Container(
-                                color: Colors.green,
-                              ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              color: Colors.green,
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
                 ),
-                Container(
-                  width: 100,
-                  color: Colors.blue,
-                ),
-              ],
-            ),
-          )
+              ),
+              Container(
+                width: 100,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
